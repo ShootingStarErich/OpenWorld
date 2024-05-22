2c308a4d86e23da948e559413dcaa29afda718c6
Ehrenberger, Erich <104063891+ShootingStarErich@users.noreply.github.com>
2024-05-22 15:42:33 +0200

    Erste Einstellungen

--- OpenWorld.java
+++ OpenWorld.java
@@ -2,8 +2,6 @@ public class OpenWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("ich liebe sie");
-
     }
 
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# OpenWorld
-An open world for my code to have fun in
